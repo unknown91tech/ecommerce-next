d3d16c4712ca61a2c86dfa947eea00910cbd6982
Jay Korat <koratjay03@gmail.com>
2023-11-29 01:15:01 +0530

    12th lec

--- playground.ts
+++ playground.ts
@@ -1,13 +1,35 @@
 
 
+interface Person {
+    name: string,
+    age: string
+}
+
+interface PersonLoggerFn {
+    (name: string , age: string) : string
+}
+
 export default function play(){
     const name = "Jay"
     const age = "20"
 
-    function logPersonInfo(personName: string, personAge: string){
+    const person: Person = {
+        name: "John",
+        age:"25"
+    }
+
+    
+    const logPersonInfo:PersonLoggerFn = (personName: string, personAge: string) =>{
         const info = "Name :" + personName + ", age: " +personAge
         console.log(info)
         return info
     }
-    logPersonInfo(name, age)
+
+    function logPersonInfo2(person : {name: string , age: string}){
+        const info = "Name :" + person.name + ", age: " +person.age
+        console.log(info)
+        return info
+    }
+    const log =logPersonInfo(name, age)
+    logPersonInfo2(person)
 }
