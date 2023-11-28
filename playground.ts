65640e686ff36798a1c952cefa719cf7926e7104
Jay Korat <koratjay03@gmail.com>
2023-11-29 01:34:12 +0530

    16th

--- playground.ts
+++ playground.ts
@@ -1,44 +1,13 @@
 
 
-// interface Person {
-//     name: string,
-//     age: string
-// }
-class Person {
-    name: string
-    age: string
-
-    constructor (name: string, age: string){
-        this.name= name
-        this.age= age
-    }
-}
-
-interface PersonLoggerFn {
-    (name: string , age?: string) : string
-}
-
 export default function play(){
-    const name = "Jay"
-    const age = "20"
-
-    const person: Person = {
-        name: "John",
-        age:"25"
+   
+    const random = Math.random()> 0.5 ? "Hello " : [1,2]
+    if(typeof random ==="string"){
+        const upper = random.toUpperCase()
     }
 
+    else console.log(random)
     
-    const logPersonInfo:PersonLoggerFn = (personName: string, personAge: string = "0") =>{
-        const info = "Name :" + personName + ", age: " +personAge
-        console.log(info)
-        return info
-    }
-
-    function logPersonInfo2(person : {name: string , age: string}){
-        const info = "Name :" + person.name + ", age: " +person.age
-        console.log(info)
-        return info
-    }
-    const log =logPersonInfo(name)
-    logPersonInfo2(new Person("Ed", "40"))
+    
 }
