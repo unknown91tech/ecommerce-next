2b9fda7ee9cfc5b565426c065b102b198b504a44
Jay Korat <koratjay03@gmail.com>
2023-11-29 14:53:37 +0530

    22nd

--- playground.ts
+++ playground.ts
@@ -3,17 +3,22 @@ import { log } from "console"
 
 
 interface Person {
+    kind : "business" | "academic" | "otherType"
     name:string
     age:number
 }
 
 interface BussinessPerson extends Person {
+    kind : "business"
     salary: number
 }
 
 interface AcademicPerson extends Person {
+    kind : "academic"
     publications: string[]
 }
+
+type Human = BussinessPerson |AcademicPerson | {kind : "otherType", special : string}
 
 type RaceCar = {
 
@@ -34,6 +39,19 @@ export default function play(){
         name: "McL",
         maxSpeed: 200,
         team: "McL"
+    }
+
+    function logPersonInfo (human: Human){
+        if(human.kind === "academic"){
+            console.log(human)
+        }else if(human.kind == "business"){
+            console.log(human)
+        }
+        else if(human.kind ==="otherType"){
+            console.log(human)
+        }
+        else console.log(human)
+        
     }
 
     function logCarInfo(car: Car){
