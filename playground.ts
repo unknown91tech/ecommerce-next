dec5521e24b63d92ae1c55b6b78e6b85621ca867
Jay Korat <koratjay03@gmail.com>
2023-11-29 14:39:14 +0530

    20th

--- playground.ts
+++ playground.ts
@@ -18,19 +18,37 @@ interface AcademicPerson extends Person {
 type RaceCar = {
 
     name: string
-    maxSpeed: number
+    maxSpeed: 200
+    team:string
 }
 
 type CityCar ={
     name: string
-    speed : number
+    space: string
+    maxSpeed : 100
 } 
 type Car = RaceCar | CityCar
 export default function play(){
     
-
+    const car: RaceCar = {
+        name: "McL",
+        maxSpeed: 200,
+        team: "McL"
+    }
 
     function logCarInfo(car: Car){
         console.log(car.name)
+
+         switch(car.maxSpeed) {
+      case 200:
+        console.log(car.team)
+        break;
+      case 100:
+        console.log(car)
+        break;
+      default:
+        console.log(car)
+    }
+
     }
 }
