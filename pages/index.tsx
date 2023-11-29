4bf8fb5c84d37e3c78d1c0a8f039cfb4cba722d6
Jay Korat <koratjay03@gmail.com>
2023-11-29 16:24:16 +0530

    47th

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 
 import type { InferGetServerSidePropsType } from "next"
+import getAllProducts from "../framework/shopify/product/get-all-products"
 
 export async function getStaticProps() {
 
-  const products = [1,2,3]
+  const products =await getAllProducts()
 
   return{
     props:{
