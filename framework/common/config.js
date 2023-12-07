4a957e72184b015a51d690847ce9ef3de7641ac3
Jay Korat <koratjay03@gmail.com>
2023-12-07 13:15:45 +0530

    65th

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,13 +1,26 @@
 
 
 const path = require("path")
+const fs = require("fs")
 const merge = require("deepmerge")
 
 function withFrameWorkConfig(defaultConfig = {}){
         const framework = "shopify"
 
-        const frameworkNextConfig = path.join("../", framework , "next.config")
+        const frameworkNextConfig = require(path.join("../", framework , "next.config"))
         const config = merge(defaultConfig, frameworkNextConfig)
+
+        const tsPath = path.join(process.cwd(), "tsconfig.json")
+        const tsConfig = require(tsPath)
+        tsConfig.compilerOptions.paths["@framework"] = [`@framework/${framework}`]
+        tsConfig.compilerOptions.paths["@framework/*"] = [`@framework/${framework}/*`]
+
+        fs.writeFileSync(
+                tsPath,
+                JSON.stringify(
+                        tsConfig, null , 2
+                )
+        )
 
         return config
 }
