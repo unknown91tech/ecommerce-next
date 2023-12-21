35ab7fde745651ba2494008e62a46645c4157cdc
Jay Korat <koratjay03@gmail.com>
2023-12-21 23:38:02 +0530

    111st

---
+++ components/ui/Marquee/Marquee.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from "react";
+import s from "./Marquee.module.css"
+interface Props {
+    children: ReactNode[]
+}
+
+const Marquee:FC<Props>= ({children}) =>{
+    return(
+        <div className={s.root}>
+            {children}
+        </div>
+    )
+}
+
+export default Marquee;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import getAllProducts from "framework/shopify/product/get-all-products"
 import getConfig from "framework/shopify/api/config"
 import { Layout } from "@components/common"
 import ProductCard from "@components/product/ProductCard"
-import { Grid, Hero } from "@components/ui"
+import { Grid, Hero, Marquee } from "@components/ui"
 
 
 export async function getStaticProps() {
@@ -29,7 +29,7 @@ export default function Home({
   
   return (
     <>
-      <Grid >
+      <Grid>
       {products.slice(0,3).map(product =>
         <ProductCard 
         key={product.id}
@@ -39,6 +39,13 @@ export default function Home({
         <Hero headline="Cookies, ice cream and muffin"
           description="Cake jelly-o danish cookie gummies shortbread jujubes. Jelly-o cake lollipop tootsie roll donut donut apple pie. Powder fruitcake soufflé topping cheesecake pudding liquorice pie danish. Tart chocolate cake gingerbread bear claw candy canes dragée. Jujubes powder pudding sweet muffin marzipan tootsie roll. Macaroon cheesecake marzipan soufflé oat cake icing caramels liquorice. Chupa chups chocolate bar tootsie roll jujubes chupa chups powder."
         />
+        <Marquee>
+        {products.slice(0,3).map(product =>
+        <ProductCard 
+        key={product.id}
+        product={product} />
+        )}
+        </Marquee>
     </>
   )
 }
