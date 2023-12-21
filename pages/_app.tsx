11a274577d2ea17a4b191b2edac31214dfc3cda6
Jay Korat <koratjay03@gmail.com>
2023-12-21 13:09:45 +0530

    101st

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,19 @@ import "@assets/main.css"
 import { AppProps } from "next/app"
 import { FC } from "react"
 
-const Noop:FC = (children) => <>{children}</>
 
-function MyApp({Component, pageProps}: AppProps & {Component: {Layout:FC}}) {
-  const  Layout=Component.Layout ?? Noop
+const Noop: FC = ({children}:any) => <>{children}</>
+
+function MyApp({Component, pageProps}: AppProps & {Component: {Layout: FC}}) {
+  const Layout = Component.Layout ?? Noop
+
   return (
-    <Layout>
-    <Component {...pageProps} />
-    </Layout>
+    
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    
   )
 }
+
 export default MyApp
