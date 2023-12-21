532e0b0ffd0ed678783b597e76b5682a8f8b931f
Jay Korat <koratjay03@gmail.com>
2023-12-21 12:00:43 +0530

    96th

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,42 +1,39 @@
 import { Product } from "@common/types/products"
 import { FC } from "react"
 import Link from "next/link"
-import Image from "next/image"
-import s from"./ProductCArd.module.css"
+import Image from "next/legacy/image"
+import s from "./ProductCArd.module.css"
 
 interface Props {
-    product: Product
+  product: Product
 }
-
 const placeholderImage = "/product-image-placeholder.svg"
 const ProductCard: FC<Props> = ({product}) => {
 
-    return (
-        <Link href={`/products/${product.slug}`}>
-        <div className={s.root}>
-            <div className={s.productTag}>
-                <h3 className={s.productTitle}>
-                    <span >{product.name}</span>
-                    
-                </h3>
-                <span className={s.productPrice}>$ 14</span>
-            </div>
+  return (
+    <Link legacyBehavior href={`/products/${product.slug}`}>
+      <a className={s.root}>
+        <div className={s.productBg}></div>
+        <div className={s.productTag}>
+          <h3 className={s.productTitle}>
+            <span>{product.name}</span>
+          </h3>
+          <span className={s.productPrice}>14 $</span>
         </div>
-            {product.images && (
-                <Image
-                    alt={product.name ?? "Product image"}
-                    src={product.images[0].url ?? placeholderImage}
-                    height={540}
-                    width={540}
-                    quality="85"
-                    layout="responsive"
-                />
-
-                
-            )}
-        
-        </Link>
-    )
+        { product.images && (
+          <Image
+            alt={product.name ?? "Product image"}
+            src={product.images[0].url ?? placeholderImage}
+            height={540}
+            width={540}
+            quality="85"
+            layout="responsive"
+          />
+        )
+        }
+      </a>
+    </Link>
+  )
 }
 
 export default ProductCard
