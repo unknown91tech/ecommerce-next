36e7f0886317bd69307f8b7daeaef8205a03280a
Jay Korat <koratjay03@gmail.com>
2023-12-22 19:45:01 +0530

    132th

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -2,15 +2,15 @@
 
 import { Layout } from "@components/common"
 import { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next"
+import getConfig from "framework/shopify/api/config"
+import getAllProductsPaths from "framework/shopify/product/get-all-products-paths"
 
 // fetch all of the products slugs
 export const getStaticPaths: GetStaticPaths = async () => {
+    const config = getConfig()
+  const { products } = await getAllProductsPaths(config)
   return {
-    paths: [
-      { params: { slug: "cool-hat"}},
-      { params: { slug: "t-shirt"}},
-      { params: { slug: "lightweight-jacket"}}
-    ],
+    paths: products.map(p => ({params: {slug: p.slug}})),
     fallback: false
   }
 }
