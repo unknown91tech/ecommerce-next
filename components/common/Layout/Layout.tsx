0fd7afb42dda38382b3c9231ffcd002c699e3bbf
Jay Korat <koratjay03@gmail.com>
2023-12-22 12:05:07 +0530

    122nd

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -3,13 +3,17 @@ import { FC } from "react"
 import style from "./Layout.module.css"
 import Footer from "../Footer"
 import Navbar from "../Navbar"
-
+import { Sidebar } from "@components/ui"
+import { CartSidebar } from "@components/cart"
 
 
 const Layout: FC = ({children}: any) => {
     return(
     <div className={style.root}>
         <Navbar/>
+        <Sidebar>
+        <CartSidebar />
+      </Sidebar>
         <main 
         className=".fit">
         {children}
--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -11,7 +11,7 @@ const Usernav:FC= () =>{
                     <Cart/>
                 </li>
                 <li className={s.item}>
-                    <Link href="/">
+                    <Link href="/wishlist">
                     <div>
                         <Heart />
                     </div>
