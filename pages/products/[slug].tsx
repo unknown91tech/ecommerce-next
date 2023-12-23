807d73b2e90f61d79b4ee8bc80164416249a58e3
Jay Korat <koratjay03@gmail.com>
2023-12-23 11:47:06 +0530

    correct 136th

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,30 +1,29 @@
-
-
 import { Layout } from "@components/common"
- import  getConfig  from "@framework/api/config"
+import  getConfig  from "@framework/api/config"
 import {
   getAllProductsPaths,
   getProduct
 } from "@framework/product"
 import { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next"
-
 // fetch all of the products slugs
 export const getStaticPaths: GetStaticPaths = async () => {
   const config = getConfig()
   const { products } = await getAllProductsPaths(config)
-
   return {
     paths: products.map(p => ({params: {slug: p.slug}})),
     fallback: false
   }
 }
-
 // provide product spefici data to the page
 export const getStaticProps = async ({
   params }: GetStaticPropsContext<{slug: string}>
 ) => {
   const config = getConfig()
-  const { product } = await getProduct(config)
+
+  const { product } = await getProduct({
+    config,
+    variables: {slug: params?.slug}
+  })
 
   return {
     props: {
@@ -32,7 +31,6 @@ export const getStaticProps = async ({
     }
   }
 }
-
 export default function ProductSlug({
   product }: InferGetStaticPropsType<typeof getStaticProps>
 ) {
@@ -43,6 +41,4 @@ export default function ProductSlug({
     </div>
   )
 }
-
-
 ProductSlug.Layout = Layout
