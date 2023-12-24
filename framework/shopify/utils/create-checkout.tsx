1ac572d8544ce4e9bf618c169875089f3e2d0e5e
Jay Korat <koratjay03@gmail.com>
2023-12-25 01:46:07 +0530

    189th

--- framework/shopify/utils/create-checkout.tsx
+++ framework/shopify/utils/create-checkout.tsx
@@ -1,16 +1,22 @@
 import { ApiFetcher } from "@common/types/api"
+import { Checkout, CheckoutCreatePayload, Maybe } from "@framework/schema"
 import { checkoutCreateMutation } from "./mutations"
 
+
+
 const createCheckout = async (
-    fetch: ApiFetcher
-  ): Promise<any> => {
-    debugger
-    const { data } = await fetch({
-      query: checkoutCreateMutation
-    })
-    debugger
+  fetch: ApiFetcher<{checkoutCreate: CheckoutCreatePayload}>
+): Promise<Maybe<Checkout | undefined>> => {
+  const { data } = await fetch({
+    query: checkoutCreateMutation
+  })
+
+  const { checkout } = data.checkoutCreate
+  const checkoutId = checkout?.id
   
-    return data
+
+  return checkout
 }
+
 
 export default createCheckout
