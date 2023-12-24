561461dde308649501f4cbe146fcdd9804fc8017
Jay Korat <koratjay03@gmail.com>
2023-12-24 14:35:58 +0530

    till 164th without 162nd

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -6,25 +6,28 @@ import Navbar from "../Navbar"
 import { Sidebar } from "@components/ui"
 import { CartSidebar } from "@components/cart"
 import { useUI } from "@components/ui/context"
+import { ApiProvider } from "@framework"
 
 
 const Layout: FC = ({children}: any) => {
     const {isSidebarOpen, closeSidebar} =useUI()
     
     return(
-    <div className={style.root}>
-        <Navbar/>
-        <Sidebar
-        onClose={closeSidebar}
-        isOpen={isSidebarOpen}>
-        <CartSidebar />
-      </Sidebar>
-        <main 
-        className=".fit">
-        {children}
-        </main>
-        <Footer />
-    </div>
+      <ApiProvider>
+        <div className={style.root}>
+            <Navbar/>
+            <Sidebar
+            onClose={closeSidebar}
+            isOpen={isSidebarOpen}>
+            <CartSidebar />
+          </Sidebar>
+            <main 
+            className=".fit">
+            {children}
+            </main>
+            <Footer />
+        </div>
+    </ApiProvider>
     )
 }
 
---
+++ framework/shopify/index.tsx
@@ -0,0 +1,22 @@
+
+import { ReactNode, useContext } from "react";
+import getConfig from "./api/config";
+import {  ApiProvider as CoreApiProvider,
+        useApiProvider as useCoreApiProvider
+} from "@common";
+
+const config = getConfig()
+interface ShopifyApiProviderProps {
+    children: ReactNode| ReactNode[]
+}
+
+export const ApiProvider = ({children}: ShopifyApiProviderProps) => {
+    
+    return(
+        <CoreApiProvider config={{...config, testKey:"testKey"}}>
+            {children}
+        </CoreApiProvider>
+    )
+}
+
+export const useApiProvider= ()=> useCoreApiProvider()
