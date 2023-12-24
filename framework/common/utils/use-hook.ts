8ae4045dc7c6dd3697530096f41c27d97bc793e0
Jay Korat <koratjay03@gmail.com>
2023-12-25 00:26:13 +0530

    183rd

--- framework/common/utils/use-hook.ts
+++ framework/common/utils/use-hook.ts
@@ -1,6 +1,7 @@
 import { useApiProvider } from "@common"
 import { ApiHooks } from "@common/types/hooks"
 import { MutationHook } from "@common/types/hooks"
+import { useState } from "react"
 
 export const useHook = (fn: (apiHooks: ApiHooks) => MutationHook) => {
     const {hooks} = useApiProvider()
@@ -24,9 +25,20 @@ export const useMutationHook = (
     })
 }
 
+const useData= () => {
+    const [data,setData]= useState(null)
+
+    if(!data){
+        setData({data: "Cart Ready"} as any)
+    }
+    return data
+}
+
 
 export const useSWRHook = (hook: any) => {
     return hook.useHook({
-        fetch: hook.fetcher
+        useData() {
+
+        }
     })
 }
