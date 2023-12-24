ed9bbfab2cd4a6a362f0a9da825ffc2267348cb5
Jay Korat <koratjay03@gmail.com>
2023-12-25 02:50:22 +0530

    191st

--- framework/shopify/utils/queries/index.ts
+++ framework/shopify/utils/queries/index.ts
@@ -3,3 +3,4 @@
 
 export { default as getAllProductsQuery } from "./get-all-products"
 export { default as getProductQuery } from "./get-product"
+export { default as getCheckoutQuery } from "./get-checkout"
